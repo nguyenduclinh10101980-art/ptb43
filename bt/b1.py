d3ef473bac07e0b70299b27c11984aecda5ba2d9
nguyenduclinh10101980-art <nguyenduclinh10101980@gmail.com>
2026-01-10 12:56:47 +0000

    homework

---
+++ bt/b1.py
@@ -0,0 +1,6 @@
+a = float(input("Nhap chieu dai (a): "))
+b = float(input("Nhap chieu rong (b): "))
+chu_vi = 2 * (a + b)
+dien_tich = a * b
+print("chu vi hinh chu nhat: ")
+print("dien tich hinh chu nhat: ")
