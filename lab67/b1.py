0ba9a0bf0e8db9e0db32ec1f22ac7d595f7eb668
nguyenduclinh10101980-art <nguyenduclinh10101980@gmail.com>
2026-01-18 08:57:33 +0000

    bai thi

---
+++ lab67/b1.py
@@ -0,0 +1,12 @@
+is_hieu:
+if n <= 1
+for i in range (2, int(n.5) + 1):
+    if n % i == 0
+ hieu = []
+ trung_hieu = 0
+for number in range(1, 100):
+    if is_hieu(number):
+        trung_hieu(number)
+    trung_hieu += number
+    print("cac so nguyen to be hon 100 la: ")
+    print(f"Tong cac so nguyen to tren la: ")
